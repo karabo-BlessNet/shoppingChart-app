c9f9c9c9d57d893a715f39edab9d8b88f68c465b
karabo-BlessNet <107536272+karabo-BlessNet@users.noreply.github.com>
2022-07-01 13:56:10 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,62 @@
+const list = [
+  {
+    id: 1,
+    title: "Syntech USB C to USB Adapter Pack of 2 USB C Male to USB3 Female Adapter Compatible with MacBook",
+    price: 89,
+    img: "https://m.media-amazon.com/images/I/71xWh67sBNL._AC_SX466_.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Seagate Portable 2TB External Hard Drive Portable HDD",
+    price: 899,
+    img: "https://m.media-amazon.com/images/I/81tjLksKixL._AC_SX679_.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "SanDisk 128GB Ultra microSDXC UHS-I Memory Card ",
+    price: 499,
+    img: "https://m.media-amazon.com/images/I/61jhzv9AQRL._AC_UL320_.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "MSI Vigor GK30 Combo White, 6-Zone RGB GK30 Gaming Keyboard & GM11 Gaming Mouse",
+    price: 799,
+    img: "https://m.media-amazon.com/images/I/71oWwRSUr2L._AC_SX679_.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Glorious Model O RGB 67g Lightweight Gaming Mouse, Matte Black (GO-Black)",
+    price: 289,
+    img: "https://m.media-amazon.com/images/I/71wmuZLI6xL._AC_SX466_.jpg",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "Comfortable Swivel Task Home Office Desk Chair High Back with Adjustable Footrest",
+    price: 2399,
+    img: "https://m.media-amazon.com/images/I/41agkueaRJL._AC_.jpg",
+    amount: 1,
+  },
+  {
+    id: 7,
+    title: "Xbox Series S",
+    price: 5899,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41YzPePn9iL._SX300_SY300_QL70_FMwebp_.jpg",
+    amount: 1,
+  },
+  {
+    id: 8,
+    title: "Playstation DualSense Wireless Controller",
+    price: 599,
+    img: "https://m.media-amazon.com/images/I/612bjwBuobS._SX522_.jpg",
+    amount: 1,
+  },
+  
+  
+];
+
+export default list;
